76a80294a4d4198c8002ad3341b08f52eb8af2a2
Abigail <119028827+abi-cvc@users.noreply.github.com>
2025-10-18 14:41:49 -0500

    Enhance Calculator class with JavaDoc comments
    Added JavaDoc comments for better code documentation.

--- src/main/java/ec/epn/edu/git/calculator/Calculator.java
+++ src/main/java/ec/epn/edu/git/calculator/Calculator.java
@@ -1,18 +1,58 @@
 package ec.epn.edu.git.calculator;
 
+/**
+ * Clase Calculator que proporciona operaciones aritméticas básicas
+ * y funcionalidad para almacenar resultados.
+ * 
+ * @author EPN
+ * @version 1.0
+ */
 public class Calculator {
+    
+    /**
+     * Variable para almacenar la respuesta de operaciones
+     */
     private int answer;
-
+    
+    /**
+     * Realiza la suma de dos números enteros.
+     * 
+     * @param a El primer sumando
+     * @param b El segundo sumando
+     * @return La suma de a + b
+     */
     public int addition(int a, int b) {
         return a + b;
     }
+    
+    /**
+     * Realiza la resta de dos números enteros.
+     * 
+     * @param a El minuendo (número al que se le resta)
+     * @param b El sustraendo (número que se resta)
+     * @return La diferencia de a - b
+     */
     public int subtration(int a, int b) {
         return a - b;
     }
+    
+    /**
+     * Realiza la multiplicación de dos números enteros.
+     * 
+     * @param a El primer factor
+     * @param b El segundo factor
+     * @return El producto de a * b
+     */
     public int multiplication(int a, int b) {
         return a * b;
     }
-
+    
+    /**
+     * Pausa la ejecución del programa durante un tiempo especificado.
+     * 
+     * @param time Tiempo de espera en milisegundos
+     * @throws RuntimeException Si el hilo es interrumpido durante la espera
+     */
     public void timeout(int time){
         try{
             Thread.sleep(time);
@@ -20,13 +60,22 @@ public class Calculator {
             throw new RuntimeException(e);
         }
     }
-
+    
+    /**
+     * Obtiene el valor almacenado en la variable answer.
+     * 
+     * @return El valor actual de answer
+     */
     public int getAnswer() {
         return answer;
     }
+    
+    /**
+     * Establece un nuevo valor para la variable answer.
+     * 
+     * @param answer El valor a almacenar
+     */
     public void setAnswer(int answer) {
         this.answer = answer;
     }
-
-
 }
